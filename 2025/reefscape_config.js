3d9468642fbd2af5d0ae203944ba9c0364f0bd35
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2025-03-03 11:50:33 -0500

    Updated Fields

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -2,8 +2,8 @@ var config_data = `
 {
   "dataFormat": "tsv",
   "title": "571 Scouting PASS 2025",
-  "page_title": "REEFSCAPE",
-  "checkboxAs": "10",
+  "page_title": "571 Scouting<br>Reefscape",
+  "checkboxAs": "YN",
   "prematch": [
     { "name": "Scouter Initials",
       "code": "s",
@@ -15,19 +15,17 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025ilpe",
+      "defaultValue": "WNE",
       "required": "true"
     },
     { "name": "Match Level",
       "code": "l",
       "type": "level",
       "choices": {
-        "qm": "Quals<br>",
-        "sf": "Semifinals<br>",
-        "f": "Finals"
+        "q": "Qualifications<br>",
+        "p": "Playoffs"
       },
-      "defaultValue": "qm",
-      "required": "true"
+      "defaultValue": "q"
     },
     { "name": "Match #",
       "code": "m",
@@ -36,33 +34,21 @@ var config_data = `
       "max": 150,
       "required": "true"
     },
-    { "name": "Robot",
-      "code": "r",
-      "type": "robot",
-      "choices": {
-        "r1": "Red-1",
-        "b1": "Blue-1<br>",
-        "r2": "Red-2",
-        "b2": "Blue-2<br>",
-        "r3": "Red-3",
-        "b3": "Blue-3"
-      },
-      "required":"true"
-    },
     { "name": "Team #",
       "code": "t",
       "type": "team",
       "min": 1,
-      "max": 99999
+      "max": 99999,
+      "required": "true"
     },
-    { "name": "Auto Start Position",
-      "code": "as",
-      "type": "clickable_image",
-      "filename": "2025/half_field.png",
-      "clickRestriction": "one",
-      "dimensions": "6 6",
-      "allowableResponses": "1 7 13 19 25 31",
-      "shape": "circle 5 black red true"
+    { "name": "Alliance",
+      "code": "r",
+      "type": "robot",
+      "choices": {
+        "r": "Red<br>",
+        "b": "Blue"
+      },
+      "defaultValue": "r"
     }
   ],
   "auton": [
@@ -85,14 +71,6 @@ var config_data = `
     { "name": "Coral L4",
       "code": "ac4",
       "type": "counter"
-    },
-    { "name": "Auto Scoring Position",
-      "code": "asp",
-      "type": "clickable_image",
-      "filename": "2025/reef.png",
-      "dimensions": "6 6",
-      "allowableResponses": "1 2 3 4 5 6 7 8 9 10 11 12 13 14 17 18 19 20 23 24 25 26 27 28 29 30 31 32 33 34 35 36",
-      "shape": "circle 5 black red true"
     },
     { "name": "Processor Score",
       "code": "aps",
@@ -128,11 +106,22 @@ var config_data = `
       "code": "tns",
       "type": "counter"
     },
-    { "name": "Pickup From",
+    { "name": "Coral<br>Pickup From",
       "code": "tpu",
       "type": "radio",
       "choices": {
         "s": "Coral Station<br>",
+        "f": "Floor<br>",
+        "b": "Both<br>",
+        "x": "Not Attempted"
+      },
+      "defaultValue": "x"
+    },
+    { "name": "Algae<br>Pickup From",
+      "code": "tpu",
+      "type": "radio",
+      "choices": {
+        "r": "Reef<br>",
         "f": "Floor<br>",
         "b": "Both<br>",
         "x": "Not Attempted"
@@ -145,10 +134,6 @@ var config_data = `
     }
   ],
   "endgame": [
-    { "name": "Barge Timer",
-      "code": "ebt",
-      "type": "timer"
-    },
     { "name": "Final Robot Status",
       "code": "efs",
       "type":"radio",
@@ -163,16 +148,13 @@ var config_data = `
     }
   ],
   "postmatch": [
-    { "name": "Attained Coopertition Pt",
+    { "name": "Attained Coopertition Point",
       "code": "cop",
       "type": "bool"
     },
     { "name": "Algae Left in Reef",
       "code": "alr",
-      "type": "number",
-      "min": 0,
-      "max": 9,
-      "defaultValue": 0
+      "type": "counter"
     },
     { "name": "Driver Skill",
       "code": "ds",
@@ -189,10 +171,9 @@ var config_data = `
       "code": "dr",
       "type": "radio",
       "choices": {
-        "b": "Below Average<br>",
+        "n": "Not Effective<br>",
         "a": "Average<br>",
-        "g": "Good<br>",
-        "e": "Excellent<br>",
+        "v": "Very Effective<br>",
         "x": "Did not play defense"
       },
       "defaultValue": "x"
@@ -213,21 +194,16 @@ var config_data = `
       "code": "die",
       "type": "bool"
     },
-    { "name": "Tippy<br>(almost tipped over)",
+    { "name": "Robot Prone<br>to Tipping Over",
       "code": "tip",
       "type": "bool"
     },
-    { "name": "Dropped Coral (>2)",
+    { "name": "Dropped Coral<br>(More than 2)",
       "code": "dc",
       "type": "bool"
     },
-    { "name": "Dropped Algae (>2)",
+    { "name": "Dropped Algae<br>(More than 2)",
       "code": "da",
-      "type": "bool"
-    },
-    { "name": "Make good<br>alliance partner?",
-      "tooltip": "Would you want this robot on your alliance in eliminations?",
-      "code": "all",
       "type": "bool"
     },
     { "name": "Comments",
